3bd6eee61de33fb12c71f334793292dc942cb7b0
Dmitry <python.developer.from.minsk@gmail.com>
2022-09-29 07:14:24 +0300

    магический метод __new__

--- metod__new__.py
+++ metod__new__.py
@@ -1,7 +1,7 @@
 class Point:
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):  # метод вызывается в момент создания класса и наследуется от базового object
         print("вызов __new__ для " + str(cls))
-        return super().__new__(cls)
+        return super().__new__(cls)  # ссылается на ам класс
 
     def __init__(self, x=0, y=0):
         print("вызов __init__ для " + str(self))
